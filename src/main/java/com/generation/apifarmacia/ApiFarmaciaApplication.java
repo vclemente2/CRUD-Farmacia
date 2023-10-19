6718b757581af206040d69e82ea7bff99a08bbb3
Vinicius Bastos <vclemente2@hotmail.com>
2023-10-19 16:22:31 -0300

    Configuração inicial do projeto.

---
+++ src/main/java/com/generation/apifarmacia/ApiFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.apifarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/apifarmacia/ApiFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.apifarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
